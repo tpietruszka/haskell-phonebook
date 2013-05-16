5cc5c2a6fc3eb37ebe79c96372b33711307c3a5d
tpietruszka <tomek.pietruszka@gmail.com>
2013-05-16 22:39:35 +0200

    porządki, usunięcie Empty Phonebook, wstęp do grup

--- Phonebook.hs
+++ Phonebook.hs
@@ -6,24 +6,23 @@ import Data.Time
 import Person
 import Date
 
+type Group = String
+
 -- Show i Read beda uzywane do zapisu do / odczytu z pliku - i niczego wiecej
-data Phonebook = Phonebook [Person] | Empty deriving (Show, Read)
+data Phonebook = Phonebook [Person] [Group] deriving (Show, Read)
 
 -- dodaje osobę do książki telefonicznej, zakłada poprawność dostarczonych danych
 addPerson :: Phonebook -> Person -> Phonebook
-addPerson Empty person = Phonebook [person]
-addPerson (Phonebook pList) person = Phonebook $ sort (person : pList)
+addPerson (Phonebook pList gList) person = Phonebook (sort (person : pList)) gList
 
 -- usuwa osobę - podaną jako cały obiekt
 removePerson :: Phonebook -> Person -> Phonebook 
-removePerson Empty _ = Empty
-removePerson (Phonebook pList) person = Phonebook [x | x <- pList, x /= person]
+removePerson (Phonebook pList gList) person = Phonebook [x | x <- pList, x /= person] gList
 
 -- zamienia jedną podaną osobę na drugą. 
 -- Jeśli pierwsza nie istnieje - generuje błąd
 editPerson :: Phonebook -> Person -> Person -> Phonebook 
-editPerson Empty _ _ = error "Próba edycji pustej książki"
-editPerson (Phonebook pList) old new
+editPerson (Phonebook pList _) old new
   | old `elem` pList	= addPerson tempBook new 
   | otherwise  		= error "Próba zmiany danych osoby, której nie ma w książce"
   where 
@@ -32,11 +31,10 @@ editPerson (Phonebook pList) old new
 -- zwraca listę osób wg zadanej wartosci i kryterium, np 
 -- findPeopleBy name book "Jan"
 findPeopleBy :: (Person -> String) -> Phonebook -> String -> [Person]
-findPeopleBy _ Empty _ = []
-findPeopleBy f (Phonebook pList) value = filter ((==value) . f) pList
+findPeopleBy f (Phonebook pList _) value = filter ((==value) . f) pList
 
 findBirthdayPeople :: Phonebook -> IO [Person]
-findBirthdayPeople (Phonebook pList) = do
+findBirthdayPeople (Phonebook pList _) = do
   c <- getCurrentTime
   let today = Date $ utctDay c
   return (filter (hasBirthday today) pList)
@@ -45,4 +43,4 @@ urodziny = stringToDate "16.05.2012"
 kowalski = Person "Jan" "Kowalski" "McDonalds" "+48654654" "asd@example.com" urodziny
 nowak  = Person "Karol" "Nowak" "Tesco" "12321232" "nowak@asd.pl" (stringToDate "11.02.1980")
 
-book = addPerson (addPerson Empty kowalski) nowak
+book = addPerson (addPerson (Phonebook [] []) kowalski) nowak
